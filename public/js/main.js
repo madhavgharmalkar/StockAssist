518654a2eb20b4268bf4386613c6d064cbf164cc
maddychan <madeline.chan.8@gmail.com>
2015-11-15 05:20:47 -0800

    changed input selector

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
 
 	$("#click").click(function(event) {
 		$.ajax({
-			url: "http://stockassist.azurewebsites.net/twitter?q="+$("#stock").val(),
+			url: "http://stockassist.azurewebsites.net/twitter?q=" + $("#input-23").val(),
 			type: 'GET'
 		})
 		.done(function(data) {
