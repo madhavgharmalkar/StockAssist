cc7d815049d8737d5d13e78fbb477ac7afd48f06
Madhav Gharmalkar <gharmalkar.madhav@gmail.com>
2015-11-14 20:16:11 -0800

    Added Express

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
