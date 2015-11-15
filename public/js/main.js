ccc857cb0c887755ce192d70133ddb6db16ade6d
Madhav Gharmalkar <gharmalkar.madhav@gmail.com>
2015-11-15 06:46:41 -0800

    Added animations
    YAY :)

--- public/js/main.js
+++ public/js/main.js
@@ -4,29 +4,42 @@ jQuery(document).ready(function($) {
 	var ctx = document.getElementById("leftChart").getContext("2d");
 
 	$("#click").click(function(event) {
+
 		$.ajax({
 			url: "http://stockassist.azurewebsites.net/twitter?q="+$("#input-23").val(),
 			type: 'GET'
 		})
 		.done(function(data) {
+
+
+			$("#twoThirds")
+			.transition({height: "50vh"});
+			$("#oneThird")
+			.transition({display: block})
+			.transition({height: "50vh"});
+
+			$("#left-bottom").transition({display: block});
+			$("#right-bottom").transition({display: block});
+
+
 			console.log(data);
 
 			$("#prediction").text(data.prediction);
 			
 			var options = {};
 			var data = [
-				{
-					value: data.positive,
-					color: "#2c3e50",
-					highlight: "#34495e",
-					label: "positive"
-				},
-				{
-					value: data.negative,
-					color: "#c0392b",
-					highlight: "#e74c3c",
-					label: "negative"	
-				}
+			{
+				value: data.positive,
+				color: "#2c3e50",
+				highlight: "#34495e",
+				label: "positive"
+			},
+			{
+				value: data.negative,
+				color: "#c0392b",
+				highlight: "#e74c3c",
+				label: "negative"	
+			}
 			]
 
 
