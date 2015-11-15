9486a33bee806f14fd12a270b4ae2a63b8426f32
Madhav Gharmalkar <gharmalkar.madhav@gmail.com>
2015-11-14 23:31:05 -0800

    Moved js/css files to public view

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function($) {
+	
+
+	$("#click").click(function(event) {
+		$.ajax({
+			url: "http://stockassist.azurewebsites.net/twitter?q="+$("#stock").val(),
+			type: 'GET'
+		})
+		.done(function() {
+			console.log("success");
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		});
+
+		$("#title").transition({top: "1vh"});
+		$("#input").transition({top: "-6vh"});
+
+
+
+	});
+
+
+
+});
+
+
+
+
+
+
